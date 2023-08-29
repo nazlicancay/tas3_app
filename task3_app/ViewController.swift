13b0cdb89af2c15921fb218e63cabc316803d78b
Nazlıcan Çay <nazlican1306@gmail.com>
2023-08-29 11:39:23 +0300

    creating app

---
+++ task3_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task3_app
+//
+//  Created by Nazlıcan Çay on 29.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
